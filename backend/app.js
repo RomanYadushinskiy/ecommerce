741ce7a9369c3358c99795e9251279b2f9f5aabd
rdush <rdush824@gmail.com>
2023-07-05 23:48:05 +0300

    backend is done

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Route imports
+const product = require("./routes/ProductRoute");
+const user = require("./routes/UserRoute");
+const order = require("./routes/OrderRoute");
+
+app.use("/api/v2", product);
+app.use("/api/v2", user);
+app.use("/api/v2", order);
+
+// It`s for ErrorHandling
+app.use(ErrorHandler);
+
+module.exports = app;
---
+++ backend/controller/ProductController.js
@@ -0,0 +1,184 @@
+const Product = require("../models/ProductModel.js");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors.js");
+const ErrorHandler = require("../utils/ErrorHandler.js");
+const Features = require("../utils/Features.js");
+
+// Create Product
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
+
+// exports.getAllProducts = (req, res) => {
+//     res.status(200).json({
+//         message: "Route is working fine"
+//     })
+// };
+
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+  const resultPerPage = 8;
+
+  const productCount = await Product.countDocuments();
+
+  const feature = new Features(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await feature.query;
+  res.status(200).json({
+    success: true,
+    products,
+  });
+});
+
+// Update Product ---Admin
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product is not found with this id", 404));
+  }
+  products = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useUnified: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+// delete Product
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product is not found with this id", 404));
+  }
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted succesfully",
+  });
+});
+
+// Single product details
+exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product is not found with this id", 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+    // productCount,
+  });
+});
+
+// Create New Review or Update the review
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.forEach((rev) => {
+      if (rev.user.toString() === req.user._id.toString())
+        (rev.rating = rating), (rev.comment = comment);
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  let avg = 0;
+
+  product.reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+
+  product.ratings = avg / product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Get All reviews of a single product
+exports.getSingleProductReviews = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product is not found with this id", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+
+// Delete Review --Admin
+exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found with this id", 404));
+  }
+
+  const reviews = product.reviews.filter(
+    (rev) => rev._id.toString() !== req.query.id.toString()
+  );
+
+  let avg = 0;
+
+  reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+
+  let ratings = 0;
+
+  if (reviews.length === 0) {
+    ratings = 0;
+  } else {
+    ratings = avg / reviews.length;
+  }
+
+  const numOfReviews = reviews.length;
+
+  await Product.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+  });
+});
